1e6e5254eba5bfa6ccb2ece84354e4bdb20fc245
subramaniannk <58900536+subramaniannk@users.noreply.github.com>
2021-03-26 20:57:26 +0530

    Separating display from business logic

--- app2.py
+++ app2.py
@@ -15,38 +15,45 @@ def display():
                            no_of_columns=tic_tac_toe.no_of_columns)
 
 
+@app2.route("/reset", methods=['POST'])
+def reset():
+    tic_tac_toe.reset()
+    return display()
+
+
+@app2.route("/switch", methods=['POST'])
+def switch():
+    if tic_tac_toe.is_relinquishing_starting_turn_possible():
+        tic_tac_toe.relinquish_starting_turn()
+    else:
+        pass
+        # flash an error message
+    return display()
+
+
 @app2.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        sub_value = request.form.get('sub')
-        if sub_value == "Reset":
-            tic_tac_toe.reset()
-            return display()
-        if sub_value == "Switch":
-            if tic_tac_toe.is_relinquishing_starting_turn_possible():
-                tic_tac_toe.relinquish_starting_turn()
-            else:
-                pass
-                #flash an error message
-            return display()
+    if request.method == 'GET':
+        return display()
 
-        sub_value_int = int(sub_value)
-        y1 = sub_value_int % 3
-        x1 = sub_value_int // 3
-        tic_tac_toe.play(x1, y1)
-        winner = tic_tac_toe.get_winner()
-        if winner is not None:
-            return render_template('victory1.html',
-                                   positions=tic_tac_toe.positions,
-                                   player=winner,
-                                   no_of_rows=tic_tac_toe.no_of_rows,
-                                   no_of_columns=tic_tac_toe.no_of_columns)
+    sub_value = request.form.get('sub')
+    sub_value_int = int(sub_value)
+    y1 = sub_value_int % 3
+    x1 = sub_value_int // 3
+    tic_tac_toe.play(x1, y1)
+    winner = tic_tac_toe.get_winner()
+    if winner is not None:
+        return render_template('victory1.html',
+                               positions=tic_tac_toe.positions,
+                               player=winner,
+                               no_of_rows=tic_tac_toe.no_of_rows,
+                               no_of_columns=tic_tac_toe.no_of_columns)
 
-        if tic_tac_toe.is_game_draw():
-            return render_template('draw1.html',
-                                   positions=tic_tac_toe.positions,
-                                   no_of_rows=tic_tac_toe.no_of_rows,
-                                   no_of_columns=tic_tac_toe.no_of_columns)
+    if tic_tac_toe.is_game_draw():
+        return render_template('draw1.html',
+                               positions=tic_tac_toe.positions,
+                               no_of_rows=tic_tac_toe.no_of_rows,
+                               no_of_columns=tic_tac_toe.no_of_columns)
 
     return display()
 
