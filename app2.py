8515e7beedab70eb9f2266f569bf9b737cb73625
subramaniannk <58900536+subramaniannk@users.noreply.github.com>
2021-03-26 16:36:18 +0530

    Separating display from business logic

--- app2.py
+++ app2.py
@@ -3,23 +3,27 @@ from flask import Flask, request, redirect, url_for, render_template
 
 app2 = Flask(__name__)
 positions = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
-x = ['x', 'o']
+players = ['x', 'o']
 
 
 @app2.route("/", methods=['GET', 'POST'])
 def index():
     print(request.method)
-    y = x[0]
+    player = players[0]
     if request.method == 'POST':
         sub_value = request.form.get('sub')
+        if sub_value == "Reset":
+            reset()
+            return redirect(url_for("fff"))
+        if sub_value == "Switch":
+            players.reverse()
+            return redirect(url_for("fff"))
+
         sub_value_int = int(sub_value)
         y1 = sub_value_int % 3
         x1 = sub_value_int // 3
-
-        positions[x1][y1] = y
-        x.reverse()
-
+        positions[x1][y1] = player
+        players.reverse()
         winner = who_won()
         if winner is not None:
             return render_template('victory.html', positions=positions, player=winner)
@@ -31,13 +35,13 @@ def index():
 
 @app2.route("/fff")
 def fff():
-    return render_template('display.html', positions=positions, player=x[0])
+    return render_template('display.html', positions=positions, player=players[0], enable=switch_conditions())
 
 
 def is_game_draw():
-    for i in positions:
-        for j in i:
-            if j == 0:
+    for rows in positions:
+        for element in rows:
+            if element == 0:
                 return False
     return True
 
@@ -47,8 +51,8 @@ def who_won_with_pattern(pattern):
     val = positions[x1][y1]
     if val == 0:
         return None
-    for (x2, y2) in pattern:
-        if val != positions[x2][y2]:
+    for (x, y) in pattern:
+        if val != positions[x][y]:
             return None
     return val
 
@@ -68,6 +72,29 @@ def who_won():
         if winner is not None:
             return winner
     return None
+
+
+def reset():
+    global positions
+    positions = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    global players
+    players = ['x', 'o']
+    return redirect(url_for("fff"))
+
+
+def switch_conditions():
+    for row in positions:
+        for element in row:
+            if element != 0:
+                return 0
+    return 1
+
+
+@app2.route("/ddd")
+def lighter():
+    return render_template("checking", positions=[[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+
+
 
 
 app2.run()
