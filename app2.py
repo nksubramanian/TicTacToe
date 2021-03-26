2ae8fd94fb18aa1af0b6e38a41a846d646de4db5
subramaniannk <58900536+subramaniannk@users.noreply.github.com>
2021-03-26 20:13:55 +0530

    Separating display from business logic

--- app2.py
+++ app2.py
@@ -1,95 +1,141 @@
 from flask import Flask, request, redirect, url_for, render_template
 
 
+class TicTacToe:
+
+    def __init__(self):
+        self.positions = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.players = ['x', 'o']
+        self.no_of_rows = 3
+        self.no_of_columns = 3
+
+    def reset(self):
+        self.positions = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.players = ['x', 'o']
+
+    def relinquish_starting_turn(self):
+        if self.is_relinquishing_starting_turn_possible():
+            self.players.reverse()
+        else:
+            raise Exception("Unsupported Operation relinquishing")
+
+    def is_relinquishing_starting_turn_possible(self):
+        for row in self.positions:
+            for element in row:
+                if element != 0:
+                    return False
+        return True
+
+    def play(self, x, y):
+        self.positions[x][y] = self.players[0]
+        self.players.reverse()
+        #have to validate if cell is already played
+
+    def who_won_with_pattern(self, pattern):
+        x1, y1 = pattern[0]
+        val = self.positions[x1][y1]
+        if val == 0:
+            return None
+        for (x, y) in pattern:
+            if val != self.positions[x][y]:
+                return None
+        return val
+
+    def get_winner(self):
+        winning_patterns = [[(0, 0), (1, 1), (2, 2)],
+                            [(0, 0), (0, 1), (0, 2)],
+                            [(1, 0), (1, 1), (1, 2)],
+                            [(2, 0), (2, 1), (2, 2)],
+                            [(0, 0), (1, 0), (2, 0)],
+                            [(0, 1), (1, 1), (2, 1)],
+                            [(0, 2), (1, 2), (2, 2)],
+                            [(0, 2), (1, 1), (2, 0)]]
+
+        for pattern in winning_patterns:
+            winner = self.who_won_with_pattern(pattern)
+            if winner is not None:
+                return winner
+        return None
+
+    def is_game_draw(self):
+        for rows in self.positions:
+            for element in rows:
+                if element == 0:
+                    return False
+        return True
+
+
+tic_tac_toe = TicTacToe()
 app2 = Flask(__name__)
-positions = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-players = ['x', 'o']
-no_of_rows = 3
-no_of_columns = 3
 
 
 @app2.route("/", methods=['GET', 'POST'])
 def index():
     print(request.method)
-    player = players[0]
+
     if request.method == 'POST':
         sub_value = request.form.get('sub')
         if sub_value == "Reset":
-            reset()
+            tic_tac_toe.reset()
             return redirect(url_for("fff"))
         if sub_value == "Switch":
-            players.reverse()
+            if tic_tac_toe.is_relinquishing_starting_turn_possible():
+                tic_tac_toe.relinquish_starting_turn()
+            else:
+                pass
+                #flash an error message
             return redirect(url_for("fff"))
 
         sub_value_int = int(sub_value)
         y1 = sub_value_int % 3
         x1 = sub_value_int // 3
-        positions[x1][y1] = player
-        players.reverse()
-        winner = who_won()
+        tic_tac_toe.play(x1, y1)
+        winner = tic_tac_toe.get_winner()
         if winner is not None:
-            return render_template('victory1.html', positions=positions, player=winner, no_of_rows=no_of_rows, no_of_columns=no_of_columns)
-        if is_game_draw():
-            return render_template('draw1.html', positions=positions, no_of_rows=no_of_rows, no_of_columns=no_of_columns)
+            return render_template('victory1.html',
+                                   positions=tic_tac_toe.positions,
+                                   player=winner,
+                                   no_of_rows=tic_tac_toe.no_of_rows,
+                                   no_of_columns=tic_tac_toe.no_of_columns)
+
+        if tic_tac_toe.is_game_draw():
+            return render_template('draw1.html',
+                                   positions=tic_tac_toe.positions,
+                                   no_of_rows=tic_tac_toe.no_of_rows,
+                                   no_of_columns=tic_tac_toe.no_of_columns)
 
     return redirect(url_for("fff"))
 
 
 @app2.route("/fff")
 def fff():
-    return render_template('display1.html', positions=positions, player=players[0], enable=switch_conditions(), no_of_rows=no_of_rows, no_of_columns=no_of_columns)
+    x = 1 if tic_tac_toe.is_relinquishing_starting_turn_possible() else 0
+    return render_template('display1.html',
+                           positions=tic_tac_toe.positions,
+                           player=tic_tac_toe.players[0],
+                           enable=x,
+                           no_of_rows=tic_tac_toe.no_of_rows,
+                           no_of_columns=tic_tac_toe.no_of_columns)
 
 
-def is_game_draw():
-    for rows in positions:
-        for element in rows:
-            if element == 0:
-                return False
-    return True
 
 
-def who_won_with_pattern(pattern):
-    x1, y1 = pattern[0]
-    val = positions[x1][y1]
-    if val == 0:
-        return None
-    for (x, y) in pattern:
-        if val != positions[x][y]:
-            return None
-    return val
 
 
-def who_won():
-    winning_patterns = [[(0, 0), (1, 1), (2, 2)],
-                        [(0, 0), (0, 1), (0, 2)],
-                        [(1, 0), (1, 1), (1, 2)],
-                        [(2, 0), (2, 1), (2, 2)],
-                        [(0, 0), (1, 0), (2, 0)],
-                        [(0, 1), (1, 1), (2, 1)],
-                        [(0, 2), (1, 2), (2, 2)],
-                        [(0, 2), (1, 1), (2, 0)]]
-
-    for pattern in winning_patterns:
-        winner = who_won_with_pattern(pattern)
-        if winner is not None:
-            return winner
-    return None
 
 
-def reset():
-    global positions
-    positions = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    global players
-    players = ['x', 'o']
-    return redirect(url_for("fff"))
 
 
-def switch_conditions():
-    for row in positions:
-        for element in row:
-            if element != 0:
-                return 0
-    return 1
+
+
+
+
+
+
+
+
+
+
 
 
 app2.run()
