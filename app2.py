66706e1ece5a480dea407707c201b6a6cde99d76
subramaniannk <58900536+subramaniannk@users.noreply.github.com>
2021-03-30 13:25:59 +0530

    Separating display from business logic

--- app2.py
+++ app2.py
@@ -1,79 +1,81 @@
 from flask import Flask, request, render_template, redirect, url_for
 from tic_tac_toe import TicTacToe
 
-#tic_tac_toe = TicTacToe(5)
+
 app2 = Flask(__name__)
 object_list = []
 
 
-@app2.route('/display/<i>')
+@app2.route('/display/<int:i>')
 def display(i):
-    ii=int(i)
-    x = 1 if object_list[ii].is_relinquishing_starting_turn_possible() else 0
+    i = int(i)
+    x = 1 if object_list[i].is_relinquishing_starting_turn_possible() else 0
     return render_template('display1.html',
-                           positions=object_list[ii].positions,
-                           player=object_list[ii].players[0],
+                           positions=object_list[i].positions,
+                           player=object_list[i].players[0],
                            enable=x,
-                           no_of_rows=object_list[ii].no_of_rows,
-                           no_of_columns=object_list[ii].no_of_columns,
-                           user=ii)
+                           no_of_rows=object_list[i].no_of_rows,
+                           no_of_columns=object_list[i].no_of_columns,
+                           user=i)
 
 
-@app2.route("/reset", methods=['POST'])
-def reset():
-    tic_tac_toe.reset()
-    return display()
+@app2.route("/reset/<int:i>", methods=['POST'])
+def reset(i):
+    object_list[i].reset()
+    return redirect(url_for("display", i=i))
 
 
-@app2.route("/switch", methods=['POST'])
-def switch():
-    if tic_tac_toe.is_relinquishing_starting_turn_possible():
-        tic_tac_toe.relinquish_starting_turn()
+@app2.route("/switch/<int:i>", methods=['POST'])
+def switch(i):
+    if object_list[i].is_relinquishing_starting_turn_possible():
+        object_list[i].relinquish_starting_turn()
     else:
         pass
         # flash an error message
-    return display()
+    return redirect(url_for("display", i=i))
 
 
-@app2.route("/<i>")
+@app2.route("/<int:i>")
 def index(i):
     return display(i)
 
 
-@app2.route("/play/<i>", methods=['POST'])
+@app2.route("/play/<int:i>", methods=['POST'])
 def play(i):
-    ii = int(i)
+    i = int(i)
     sub_value = request.form.get('sub')
     sub_value_int = int(sub_value)
     y1 = sub_value_int % 3
     x1 = sub_value_int // 3
-    object_list[ii].play(x1, y1)
-    winner = object_list[ii].get_winner()
+    object_list[i].play(x1, y1)
+    winner = object_list[i].get_winner()
     if winner is not None:
         return render_template('victory1.html',
-                               positions=tic_tac_toe.positions,
+                               positions=object_list[i].positions,
                                player=winner,
-                               no_of_rows=tic_tac_toe.no_of_rows,
-                               no_of_columns=tic_tac_toe.no_of_columns)
+                               no_of_rows=object_list[i].no_of_rows,
+                               no_of_columns=object_list[i].no_of_columns,
+                               user=i)
 
-    if tic_tac_toe.is_game_draw():
+    if object_list[i].is_game_draw():
         return render_template('draw1.html',
-                               positions=tic_tac_toe.positions,
-                               no_of_rows=tic_tac_toe.no_of_rows,
-                               no_of_columns=tic_tac_toe.no_of_columns)
+                               positions=object_list[i].positions,
+                               no_of_rows=object_list[i].no_of_rows,
+                               no_of_columns=object_list[i].no_of_columns,
+                               user=i)
 
-    return redirect(url_for("index",i = i))
+    return redirect(url_for("index", i=i))
 
 
 @app2.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
         user = request.form['nm']
-        global tic_tac_toe
-        tic_tac_toe = TicTacToe(user)
+        #global tic_tac_toe
+        #tic_tac_toe = TicTacToe(user)
         object_list.append(TicTacToe(user))
         return None
-        #return temp()
+
 
 
 @app2.route("/home")
