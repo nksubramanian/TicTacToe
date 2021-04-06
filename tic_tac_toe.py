6f3ef823e53f4ea577353cb8a27b993ab865fd58
subramaniannk <58900536+subramaniannk@users.noreply.github.com>
2021-04-06 16:52:33 +0530

    refreshing periodically is done

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -39,7 +39,7 @@ class TicTacToe:
                 return None
         return val
 
-    def get_winner(self):
+    def (self):
         winning_patterns = [[(0, 0), (1, 1), (2, 2)],
                             [(0, 0), (0, 1), (0, 2)],
                             [(1, 0), (1, 1), (1, 2)],
